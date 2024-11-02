5050fea9351fd5dbb6a1781ab9bfe9e1d067e057
cherry <cherry20102024@gmail.com>
2024-11-02 13:05:01 +0530

    updated app2.py

---
+++ app2.py
@@ -0,0 +1,10 @@
+import boto3
+client = boto3.client('ec2')
+response = client.run_instances(
+   ImageId='ami-06b21ccaeff8cd686',
+   InstanceType= 't2.medium',
+   KeyName='eksk8s',
+   MaxCount=1,
+   MinCount=1
+)
+
